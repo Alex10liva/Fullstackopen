4f1f60e93b2ace48dca834e5a4111be0b9f0a4fd
Alex10liva <alex_oliv1000@hotmail.com>
2023-07-05 19:25:57 -0600

    features added to eslint

--- part3/phonebook/.eslintrc.js
+++ part3/phonebook/.eslintrc.js
@@ -23,5 +23,10 @@ module.exports = {
     indent: ["error", 2],
     quotes: ["error", "double"],
     semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
   },
 };
